a630fe2af3cde4d92acc14b450c96abfa687842e
Александр <alextrexen@gmail.com>
2025-09-22 14:27:06 +0300

    Планшетная версия v1.0.0

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,9 +1,10 @@
+const mainNav = document.querySelector('.main-nav');
 const openMenu = document.querySelector('.main-nav__button--open');
 const closeMenu = document.querySelector('.main-nav__button--close');
-
 const menuBlock = document.querySelector('.main-nav__block');
-
 const navLinks = document.querySelectorAll('.main-nav__link');
+
+// Закрытие мобильного меню после нажатия пункта
 
 navLinks.forEach(link => {
   link.addEventListener('click', () => {
@@ -11,10 +12,33 @@ navLinks.forEach(link => {
   });
 });
 
+// Открытие меню
+
 openMenu.addEventListener('click', () => {
   menuBlock.classList.add('main-nav__block--open');
 });
 
+// Закрытие меню
+
 closeMenu.addEventListener('click', () => {
   menuBlock.classList.remove('main-nav__block--open');
 });
+
+if (window.matchMedia("(min-width: 768px)").matches) {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY >= 650) {
+      mainNav.classList.add('main-nav--scroll');
+    } else {
+      mainNav.classList.remove('main-nav--scroll');
+    }
+  });
+}
+
+
+// window.addEventListener('scroll', () => {
+//   if (window.scrollY >= 650) {
+//     mainNav.classList.add('main-nav--scroll');
+//   } else {
+//     mainNav.classList.remove('main-nav--scroll');
+//   }
+// });
